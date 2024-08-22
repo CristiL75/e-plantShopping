d1db55848e7a95744c0c4803c6f140ad88ac1422
CristiL75 <cristilatcu7@yahoo.com>
2024-08-22 09:29:50 -0400

    Initial commit with ProductList component and set

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './store.js';
+
+// Render the root element with React.StrictMode for highlighting potential problems in the application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Provide the Redux store to the entire application */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
-)
+);
